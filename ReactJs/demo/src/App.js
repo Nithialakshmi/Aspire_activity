d407ccd7642d49562ed03ca3dbe97abf2c76ec1c
Nithialakshmi <nithialakshmi@gmail.com>
2024-06-12 22:18:34 +0530

    Updated React Files

---
+++ ReactJs/demo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MountingDemo from './MountingDemo';
+
+function App() {
+  return (
+    <div className="App">
+      <MountingDemo></MountingDemo>
+     </div>
+  );
+}
+
+export default App;
